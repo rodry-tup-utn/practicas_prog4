864e9f9e5543c29d99a1b7c030f47b694dd915d2
rodry <rodrymza@gmail.com>
2026-05-30 01:07:08 -0300

    feat: list responde con paginacion y filtrado, eliminacion de commit, funcion auxiliar para verificar existencia

--- server/app/modules/category/service.py
+++ server/app/modules/category/service.py
@@ -1,6 +1,12 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from app.modules.category.model import Category
-from app.modules.category.schemas import CategoryCreate, CategoryUpdate
+from app.modules.category.schemas import (
+    CategoryCreate,
+    CategoryUpdate,
+    CategoryPaginated,
+    CategoryFilters,
+    CategoryRead,
+)
 from app.core.unit_of_work import UnitOfWork
 
 
@@ -8,32 +14,38 @@ class CategoryService:
     def __init__(self, uow: UnitOfWork):
         self.uow = uow
 
-    def list(self) -> list[Category]:
-        return list(self.uow.categories.get_all())
+    def list_all(self, filters: CategoryFilters) -> CategoryPaginated:
+        data = self.uow.categories.get_all(filters)
+        total = self.uow.categories.count(filters)
+
+        items = [CategoryRead.model_validate(c) for c in data]
+
+        return CategoryPaginated(items=items, total=total)
+
+    def _get_or_404(self, id: int):
+        category = self.uow.categories.get_by_id(id)
+        if not category:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND, f"Categoria {id} no encontrada"
+            )
+        return category
 
     def get_by_id(self, id: int) -> Category | None:
-        return self.uow.categories.get_by_id(id)
+        return self._get_or_404(id)
 
     def create(self, data: CategoryCreate) -> Category:
         category = Category.model_validate(data)
         self.uow.categories.add(category)
-        self.uow.commit()
         return category
 
     def update(self, id: int, data: CategoryUpdate) -> Category:
-        category = self.uow.categories.get_by_id(id)
-        if not category:
-            raise HTTPException(status_code=404, detail="Categoría no encontrada")
+        category = self._get_or_404(id)
         update_data = data.model_dump(exclude_unset=True)
         for key, value in update_data.items():
             setattr(category, key, value)
-        self.uow.commit()
         self.uow.session.refresh(category)
         return category
 
     def delete(self, id: int) -> None:
-        category = self.uow.categories.get_by_id(id)
-        if not category:
-            raise HTTPException(status_code=404, detail="Categoría no encontrada")
+        category = self._get_or_404(id)
         self.uow.categories.delete(category)
-        self.uow.commit()
