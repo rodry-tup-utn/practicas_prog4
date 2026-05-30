7b38dc4d245f72b76bed4711ef5fbecb7e4672af
rodry <rodrymza@gmail.com>
2026-05-30 00:22:17 -0300

    feat: agregado lifespan para crear tablas

--- server/app/main.py
+++ server/app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.modules.product.router import router as product_router
 from app.modules.category.router import router as category_router
+from contextlib import asynccontextmanager
+from app.core.database import create_db_and_tables
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
