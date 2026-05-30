4130ea6ec6f775748d24231504e88f6fb5264423
rodry <rodrymza@gmail.com>
2026-05-30 01:08:50 -0300

    feat: filtrado avanzado, eliminacion de commit , no es necesario aqui

--- server/app/modules/product/service.py
+++ server/app/modules/product/service.py
@@ -1,57 +1,59 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from app.modules.product.model import Product
 from app.modules.category.model import Category
 from app.modules.product.schemas import (
-    ProductoCreate,
-    ProductoUpdate,
-    UpdateActive,
+    ProductCreate,
+    ProductUpdate,
     ProductFilters,
+    ProductPaginated,
+    ProductRead,
 )
 from app.core.unit_of_work import UnitOfWork
 
 
-class ProductoService:
+class ProductService:
     def __init__(self, uow: UnitOfWork):
         self.uow = uow
 
-    def list(self, filters: ProductFilters | None = None) -> list[Product]:
-        return self.uow.products.get_all(filters)
+    def list_all(self, filters: ProductFilters) -> ProductPaginated:
+        data = self.uow.products.get_all(filters)
+        total = self.uow.products.count(filters)
+        items = [ProductRead.model_validate(p) for p in data]
+
+        return ProductPaginated(items=items, total=total)
+
+    def _get_or_404(self, id: int):
+        product = self.uow.products.get_by_id_with_details(id)
+        if not product:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND, f"Producto id {id} no encontrado"
+            )
+        return product
 
     def get_by_id(self, id: int) -> Product | None:
-        return self.uow.products.get_by_id_with_details(id)
+        product = self._get_or_404(id)
 
-    def create(self, data: ProductoCreate) -> Product:
-        producto = Product.model_validate(data)
+        return product
+
+    def create(self, data: ProductCreate) -> Product:
+        product = Product.model_validate(data)
         for cat_id in data.category_ids:
             cat = self.uow.session.get(Category, cat_id)
             if cat:
-                producto.categories.append(cat)
-        self.uow.products.add(producto)
-        self.uow.commit()
-        self.uow.session.refresh(producto)
-        return producto
+                product.categories.append(cat)
+        self.uow.products.add(product)
+        self.uow.session.refresh(product)
+        return product
 
-    def update(self, id: int, data: ProductoUpdate) -> Product:
-        producto = self.uow.products.get_by_id_with_details(id)
-        if not producto:
-            raise HTTPException(status_code=404, detail="Producto no encontrado")
+    def update(self, id: int, data: ProductUpdate) -> Product:
+        product = self._get_or_404(id)
         update_data = data.model_dump(exclude_unset=True)
         for key, value in update_data.items():
-            setattr(producto, key, value)
-        self.uow.products.save(producto)
-        return producto
-
-    def update_active(self, id: int, data: UpdateActive) -> Product:
-        producto = self.uow.products.get_by_id(id)
-        if not producto:
-            raise HTTPException(status_code=404, detail="Producto no encontrado")
-        producto.active = data.active
-        self.uow.products.save(producto)
-        return producto
+            setattr(product, key, value)
+        self.uow.products.add(product)
+        return product
 
     def delete(self, id: int) -> None:
-        producto = self.uow.products.get_by_id(id)
-        if not producto:
-            raise HTTPException(status_code=404, detail="Producto no encontrado")
+        producto = self._get_or_404(id)
+
         self.uow.products.delete(producto)
-        self.uow.commit()
